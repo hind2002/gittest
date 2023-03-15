f7882d212382c6d0abcd0648658035d710562db9
hind2002 <hanedhind951@gmail.com>
2023-03-15 01:28:27 +0100

    test2

--- gitproject2.py
+++ gitproject2.py
@@ -1,9 +1,20 @@
-# Demander à l'utilisateur de saisir les deux nombres
-a = float(input("Entrez le premier nombre : "))
-b = float(input("Entrez le deuxième nombre : "))
+def soustraction():
+    """
+    Ce programme demande à l'utilisateur de saisir deux nombres et calcule la soustraction de ces deux nombres.
+    """
+    # Demander à l'utilisateur de saisir les deux nombres
+    premier_nombre = float(input("Entrez le premier nombre : "))
+    deuxieme_nombre = float(input("Entrez le deuxième nombre : "))
 
-# Calculer la soustraction de deux nombres
-resultat = a - b
+    # Vérifier si le deuxième nombre n'est pas zéro
+    if deuxieme_nombre == 0:
+        print("Le deuxième nombre ne peut pas être zéro. Veuillez réessayer.")
+    else:
+        # Calculer la soustraction de deux nombres
+        resultat = premier_nombre - deuxieme_nombre
 
-# Afficher le résultat
-print("Le résultat de la soustraction est :", resultat)
+        # Afficher le résultat
+        print("Le résultat de la soustraction est :", resultat)
+
+# Appeler la fonction pour exécuter le programme
+soustraction()
